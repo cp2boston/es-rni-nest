22e626251340eac1f1dc4123e3f8a3ac0c4c5388
Chris Park <cpark@basistech.com>
2016-11-02 07:22:56 -0700

    Updates
    - Added blank README.me
    - Added RNIBase class to provide map formatting
    - Updated comments and variable names in Program.cs

--- estest/Program.cs
+++ estest/Program.cs
@@ -7,13 +7,12 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Nest;
 
-using rni_name = System.String; // For readability only, it has no underlying effect on results
-
 namespace estest {
     class Program {
         static void Main(string[] args) {
             string indexName = "rni-test";
-            var node = new Uri("http://10.1.9.206:32773");
+            string esHostURL = "http://10.1.9.206:32773"; // Set to the host:port of elasticsearch
+            var node = new Uri(esHostURL);
             var settings = new ConnectionSettings(node)
                 .DefaultIndex(indexName);
             var client = new ElasticClient(settings);
@@ -22,38 +21,25 @@ namespace estest {
 
             var createResponse = client.CreateIndex(indexName);
 
-
-            // Map custom type
-            // Since the RNI plugin requires a custom type, rni_name, and the fluent NEST model doesn't appear to 
-            // have a way to map a custom type, use the LowLevel feature of NEST to apply the correct mapping.
-            // This only needs to be done once.
-
-            // Building the JSON using Newtonsoft is the safest way to ensure that the resulting JSON is correctly formatted
-            JObject body = new JObject(
-                new JProperty("record",
-                    new JObject(
-                        new JProperty("properties",
-                            new JObject(
-                                new JProperty("FullName", new JObject( new JProperty("type", "rni_name")) ),
-                                new JProperty("LocalName", new JObject( new JProperty("type", "rni_name")) ),
-                                new JProperty("DateOfBirth", new JObject( new JProperty("type", "rni_date") ))
-                            )
-                        )
-                    )
-                )
-            );
-            // Use LowLevel to map the custom types
-            var mapResponse = client.LowLevel.IndicesPutMapping<Record>(indexName, "record", body.ToString());
-
-            // Add the sample record
-            Record record = new estest.Program.Record {
+            // Create a sample record
+            Record sampleRecord = new estest.Program.Record {
                 Id = "1",
                 FullName = "Joe Schmoe",
                 LocalName = "Joe the Schmoe",
                 DateOfBirth = new DateTime(1980, 11, 11)
             };
 
-            var indexResponse = client.Index(record);
+            // Map custom type
+            // Since the RNI plugin requires a custom type, rni_name, and the fluent NEST model doesn't appear to 
+            // have a way to map a custom type, use the LowLevel feature of NEST to apply the correct mapping.
+            // This only needs to be done once.
+            // NOTE: The type, e.g. Record and the second argument, Type, should match in case.  Why the IndicesPutMapping
+            //       method can't figure out the type from the generic is a mystery.
+            var mapResponse = client.LowLevel.IndicesPutMapping<Record>(indexName, "Record", sampleRecord.MapToRNITypes());
+
+
+
+            var indexResponse = client.Index(sampleRecord);
 
             // This is here to make sure that the record was added.  It's not necessary for operation.
             var getResponse = client.Get<Record>(1, idx => idx.Index(indexName));
@@ -101,13 +87,13 @@ namespace estest {
             client.DeleteIndex(indexName);
         }
         /// <summary>
-        /// Sample record to use for testing.  Note that "rni_name" is just an alias for string.  Under the hood
-        /// it gets resolved to string, so its only value is readability.
+        /// Sample record to use for testing.  Derive from RNIBase to use the
+        /// custom type mapping method.
         /// </summary>
-        public class Record {
+        public class Record : RNIBase {
             public string Id { get; set; }
-            public rni_name FullName { get; set; }
-            public rni_name LocalName { get; set; }
+            public string FullName { get; set; }
+            public string LocalName { get; set; }
             public DateTime DateOfBirth { get; set; }
         }
         
---
+++ estest/RNIBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace estest {
+    /// <summary>
+    /// RNIBase provides a base class that provides RNI specific methods to inherited classes.  Specifically, it:
+    ///     - provides a JSON formatter to properly format the public properties of a child class for use in
+    ///       LowLevel mapping of the schema.
+    /// </summary>
+    public class RNIBase {
+        public RNIBase() { }
+
+        /// <summary>
+        /// Returns a properly formatted JSON string that can be used for LowLevel NEST mapping
+        /// 
+        /// </summary>
+        /// <returns>Properly formatted JSON string</returns>
+        public string MapToRNITypes() {
+            var className = this.GetType().Name;
+            var publicProperties = this.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+
+            // Building the JSON using Newtonsoft is the safest way to ensure that the resulting JSON is correctly formatted
+            JObject body = new JObject(
+                new JProperty(className,
+                    new JObject(
+                        new JProperty("properties",
+                            new JObject (
+                            from property in publicProperties //.Where( t=> !t.Name.Contains("Id") )
+                                select new JProperty(property.Name, new JObject(new JProperty("type", rniType(property.PropertyType.Name))))
+                            )
+                        )
+                    )
+                )
+            );
+            return body.ToString();
+        }
+
+        /// <summary>
+        /// Maps the standard type to the RNI Type
+        /// </summary>
+        /// <param name="propertyType">Standard type</param>
+        /// <returns>RNI type</returns>
+        private string rniType(string propertyType) {
+            Dictionary<string, string> rniTypes = new Dictionary<string, string>() {
+                { "String", "rni_name" },
+                { "DateTime", "rni_date" }
+            };
+
+            if (rniTypes.ContainsKey(propertyType)) {
+                return rniTypes[propertyType];
+            }
+            else {
+                return propertyType;
+            }
+        }
+    }
+}
