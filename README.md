86bdcfff4ff62cbefefad3e8c984f5335aeb6582
Chris Park <cpark@basistech.com>
2017-06-21 07:05:18 -0700

    Updated to run with ES 5.3.1

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Rosette for Elasticsearch + NEST
 Using the Basis Technology Elasticsearch RNI plugin with NEST can be a bit confusing due to the use of custom types, rni-name and rni-date, and a custom function_score, name_score.
 
-The code in this repo provides an example of how to use NEST's lowlevel and raw functions to properly map a class to the RNI types and to perform a name_score query. 
+The code in this repo provides an example of how to use NEST's lowlevel and raw functions to properly map a class to the RNI types and to perform a name_score query.
 ### Requirements
-- Elasticsearch
+- Elasticsearch 5.3
 - Rosette for Elasticsearch plugin and license (see [Basis Technology](https://www.rosette.com/elastic/))
-- Nuget NEST
+- Nuget NEST 5.3.1
+- Nuget Elasticsearch.Net 5.3.1
 
 ### Code
 - Program.cs - simple console application that demonstrates a working name_score query
--- estest/Program.cs
+++ estest/Program.cs
@@ -1,27 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using Nest;
+using Newtonsoft.Json;
+using Elasticsearch.Net;
 
 namespace estest {
     class Program {
         static void Main(string[] args) {
             string indexName = "rni-test";
-            string esHostURL = "http://10.1.9.206:32773"; // Set to the host:port of elasticsearch
-            var node = new Uri(esHostURL);
-            var settings = new ConnectionSettings(node)
-                .DefaultIndex(indexName);
-            var client = new ElasticClient(settings);
+            string esHostURL = "http://10.1.1.169:9200"; // Set to the host:port of elasticsearch
+            var pool = new SingleNodeConnectionPool(new Uri(esHostURL));
+            // Add a custom JSON serializer to the connection settings in order to force a
+            // conforming date format
+            var connectionSettings = new ConnectionSettings(pool, settings => new RniJsonNetSerializer(settings))
+                .DefaultIndex(indexName)
+                // Add the following to make sure that class property names are left as-is,
+                // otherwise, they will be camelCased by NEST, causing confusion with the low-level mapping
+                .DefaultFieldNameInferrer(p => p);
+            var client = new ElasticClient(connectionSettings);
+
+            Console.WriteLine("Checking for existing index");
+            if (client.IndexExists(indexName).Exists) {
+                Console.WriteLine("Deleting index");
+                client.DeleteIndex(indexName);
+            }
 
 
-
+            Console.WriteLine("Creating Index");
             var createResponse = client.CreateIndex(indexName);
 
-            // Create a sample record
+            Console.WriteLine("Creating Record");
             Record sampleRecord = new estest.Program.Record {
                 Id = "1",
                 FullName = "Joe Schmoe",
@@ -30,18 +39,20 @@ namespace estest {
             };
 
             // Map custom type
-            // Since the RNI plugin requires a custom type, rni_name, and the fluent NEST model doesn't appear to 
+            // Since the RNI plugin requires a custom type, rni_name, and the fluent NEST model doesn't appear to
             // have a way to map a custom type, use the LowLevel feature of NEST to apply the correct mapping.
             // This only needs to be done once.
             // NOTE: The type, e.g. Record and the second argument, Type, should match in case.  Why the IndicesPutMapping
             //       method can't figure out the type from the generic is a mystery.
+            Console.WriteLine("Low-Level Map");
             var mapResponse = client.LowLevel.IndicesPutMapping<Record>(indexName, "Record", sampleRecord.MapToRNITypes());
 
 
-
+            Console.WriteLine("Indexing");
             var indexResponse = client.Index(sampleRecord);
 
             // This is here to make sure that the record was added.  It's not necessary for operation.
+            Console.WriteLine("Retrieve record as a check");
             var getResponse = client.Get<Record>(1, idx => idx.Index(indexName));
 
             // The search is the other area in which a lowlevel query is needed.  Rather than use LowLevel exclusively,
@@ -61,6 +72,8 @@ namespace estest {
                     )
                 )
             );
+
+            Console.WriteLine("Perform Search");
             var searchResponse = client.Search<Record>(search => search
                 .From(0)
                 .Size(100)
@@ -68,24 +81,50 @@ namespace estest {
                     .Match(m => m.Field(f => f.FullName).Query("Joe Schmoe"))
 
                 )
-                .Rescore(rescore => rescore
-                    .WindowSize(200)
-                    .RescoreQuery(rescore_query => rescore_query
-                        .QueryWeight(0.0)
-                        .RescoreQueryWeight(1.0)
-                        .Query(query => query
-                            .Raw(customQuery.ToString())  // Raw required for custom function_score query
+                .Rescore(fn => new RescoringDescriptor<Record>()
+                    .Rescore(rescore => rescore
+                        .WindowSize(200)
+                        .RescoreQuery(rescore_query => rescore_query
+                            .QueryWeight(0.0)
+                            .RescoreQueryWeight(1.0)
+                            .Query(query => query
+                                .Raw(customQuery.ToString())  // Raw required for custom function_score query
+                            )
                         )
                     )
                 )
-
             );
 
+            Console.WriteLine(searchResponse.ToString());
             System.Diagnostics.Debug.WriteLine(searchResponse.ToString());
 
             // cleanup
+            Console.WriteLine("Remove Index");
             client.DeleteIndex(indexName);
         }
+
+        /// <summary>
+        /// Custom JsonNetSerializer to specify the date format.  RNI currently only supports date, not time, and
+        /// the format needs to be in a recognized ElasticSearch format.
+        /// </summary>
+        public class RniJsonNetSerializer : JsonNetSerializer {
+            public RniJsonNetSerializer(IConnectionSettingsValues settings) : base(settings) { }
+
+            protected override System.Collections.Generic.IList<Func<Type, JsonConverter>> ContractConverters => new System.Collections.Generic.List<Func<Type, JsonConverter>> { t => {
+                if (t == typeof(DateTime) ||
+                    t == typeof(DateTime?) ||
+                    t == typeof(DateTimeOffset) ||
+                    t == typeof(DateTimeOffset?)) {
+                        return new  Newtonsoft.Json.Converters.IsoDateTimeConverter {
+                            DateTimeFormat = "yyyy-MM-dd"
+                        };
+                    }
+
+                    return null;
+                }
+            };
+        }
+
         /// <summary>
         /// Sample record to use for testing.  Derive from RNIBase to use the
         /// custom type mapping method.
@@ -96,6 +135,6 @@ namespace estest {
             public string LocalName { get; set; }
             public DateTime DateOfBirth { get; set; }
         }
-        
+
     }
 }
